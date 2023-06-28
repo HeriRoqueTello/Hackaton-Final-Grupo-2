bfa2ef45748f149176dd8de5a7ca3d772f3d2552
Heri Roque <heriroque.22@gmail.com>
2023-06-27 19:29:34 -0500

    Add home folder

--- src/components/general/Navbar.vue
+++ src/components/general/Navbar.vue
@@ -2,20 +2,25 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <nav class=" flex flex-row h-[60px] justify-around items-center bg-[#3B00FF] text-white">
-    <img class="h-[34px]" src="/Logo.svg" alt="Logo">
+    <RouterLink to="/">
+      <img class="h-[34px]" src="/Logo.svg" alt="Logo">
+    </RouterLink>
     <div class="flex flex-row gap-4">
-      <RouterLink to="/">Home</RouterLink>
+      <!-- <RouterLink to="/">Home</RouterLink> -->
       <!-- <RouterLink to="/about">About</RouterLink> -->
       <CartIcon />
+      <MenuIcon />
     </div>
   </nav>
 </template>
 <script>
 import CartIcon from '../Icons/CartIcon.vue';
+import MenuIcon from '../Icons/MenuIcon.vue';
 
 export default {
   components: {
-    CartIcon
+    CartIcon,
+    MenuIcon
   }
 }
 </script>
