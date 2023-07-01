6c17643a8ed4ab396884e24f6fa7bc58e5f8f76a
Heri Roque <heriroque.22@gmail.com>
2023-07-01 09:53:58 -0500

    Agregar el componente ShoppingList al CheckoutView

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -4,16 +4,18 @@
       <Navbar :color="'bg-secondary'" />
     </div>
   </header>
-  <div>
-    
+  <div class="px-20 py-10">
+    <ShoppingList class="" />
   </div>
 </template>
 <script>
 import Navbar from '@/components/general/Navbar.vue';
+import ShoppingList from '@/components/shop/ShoppingList.vue';
 
 export default {
   components: {
     Navbar,
+    ShoppingList
   }  
 }
 </script>
