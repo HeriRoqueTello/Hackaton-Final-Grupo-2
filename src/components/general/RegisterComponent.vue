476df038e1e25650058573fd254109bee0fdb166
Paulo Quispe <paulo.quispe.horna@gmail.com>
2023-07-05 21:09:26 -0500

    Validaciones del registro y creacion del store para enviar datos

--- src/components/general/RegisterComponent.vue
+++ src/components/general/RegisterComponent.vue
@@ -36,69 +36,130 @@
         </p>
       </div>
 
-      <form class="mt-[40px]">
+      <Form @submit="registerStore.onSubmit(newUser)" class="mt-[40px]" :validation-schema="schema" v-slot="{ errors, isSubmitting }">
         <div
-          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] mb-[20px]">
-          <input type="text" name="nameLastname" id="nameLastname"
-            class="w-full text-xs font-bold text-[#222222] outline-0" autocomplete="off" required>
-          <label for="nameLastname" class="Roboto">
-            Nombres y apellidos
+          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px]"
+          :class="{ 'border-[#F22A40]': errors.name }">
+          <Field type="text" name="name" id="name" class="w-full text-xs font-bold text-[#222222] outline-0"
+            autocomplete="off" required v-model="newUser.name" />
+          <label for="name" class="Roboto">
+            Nombres
           </label>
         </div>
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.name }}</p>
+
         <div
-          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] mb-[20px]">
-          <input type="email" name="email" id="email" class="w-full text-xs font-bold text-[#222222] outline-0"
-            autocomplete="off" required>
+          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px]"
+          :class="{ 'border-[#F22A40]': errors.lastname }">
+          <Field name="lastname" id="lastname" class="w-full text-xs font-bold text-[#222222] outline-0"
+            autocomplete="off" required v-model="newUser.lastname" />
+          <label for="lastname" class="Roboto">
+            Apellidos
+          </label>
+        </div>
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.lastname }}</p>
+
+        <div
+          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px]"
+          :class="{ 'border-[#F22A40]': errors.email }">
+          <Field type="text" name="email" id="email" class="w-full text-xs font-bold text-[#222222] outline-0"
+            autocomplete="off" required v-model="newUser.email" />
           <label for="email" class="Roboto">
             Correo electrónico
           </label>
         </div>
-        <div class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] mb-[20px] flex justify-between items-center">
-          <input type="password" name="password01" id="password01"
-            class="w-full text-xs font-bold text-[#222222] outline-0" autocomplete="off" required
-            v-model="valuePassword01">
-          <label for="password01">Contraseña</label>
-          <img class="w-[16px] h-[11px] cursor-pointer" src="/icons/show-icon.png" alt="Show Password Icon"
-            @click="showPassword01 = true">
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.email }}</p>
+
+        <div
+          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] flex justify-between items-center"
+          :class="{ 'border-[#F22A40]': errors.password01 }">
+          <Field :type="typePassword01" name="password01" id="password01"
+            class="w-full text-xs font-bold text-[#222222] outline-0" autocomplete="off" required v-model="newUser.password" />
+          <label for="password01" class="Roboto">
+            Contraseña
+          </label>
+          <img class="w-[16px] h-[11px] cursor-pointer" src="/icons/show-icon.png" alt="Show Password Icon" @click="typePassword01 === 'password' ? typePassword01 = 'text' : typePassword01 = 'password'">
         </div>
-        <div class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] mb-[20px] flex justify-between items-center">
-          <input type="password" name="password02" id="password02"
-            class="w-full text-xs font-bold text-[#222222] outline-0" autocomplete="off" required
-            v-model="valuePassword02">
-          <label for="password02">
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.password01 }}</p>
+
+        <div
+          class="relative w-full border-solid border-[1px] border-[#8b9099] rounded-[5px] pt-[20px] px-[15px] pb-[16px] flex justify-between items-center"
+          :class="{ 'border-[#F22A40]': errors.password02 }">
+          <Field :type="typePassword02" name="password02" id="password02"
+            class="w-full text-xs font-bold text-[#222222] outline-0" autocomplete="off" required />
+          <label for="password02" class="Roboto">
             Repite contraseña
           </label>
-          <img class="w-[16px] h-[11px] cursor-pointer" src="/icons/show-icon.png" alt="Show Password Icon"
-            @click="showPassword02 = true">
+          <img class="w-[16px] h-[11px] cursor-pointer" src="/icons/show-icon.png" alt="Show Password Icon" @click="typePassword02 === 'password' ? typePassword02 = 'text' : typePassword02 = 'password'">
         </div>
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.password02 }}</p>
+
         <div class="flex items-center">
-          <input type="checkbox" name="accept" id="accept" class="my-checkbox" required><label for="accept"
-            class="text-xs text-[#222222] ml-[10px]">Acepto <a class="font-bold underline">Términos y
-              Condiciones</a></label>
+          <Field type="checkbox" name="acceptTerms" id="acceptTerms" class="my-checkbox" required /><label
+            for="acceptTerms" class="text-xs text-[#222222] ml-[10px]">
+            Acepto <a href="/terminos" class="font-bold underline" target="_blank"> Términos y Condiciones</a>
+          </label>
         </div>
-        <button class="mt-[30px] mb-[60px] bg-[#5640FF] w-full rounded-[10px] py-[18px] text-sm font-bold text-[#ffffff]">
+        <p class="mb-[20px] text-xs text-right text-[#F22A40]">{{ errors.acceptTerms }}</p>
+
+        <button :disabled="isSubmitting"
+          class="mt-[30px] mb-[60px] bg-[#5640FF] w-full rounded-[10px] py-[18px] text-sm font-bold text-[#ffffff] Poppins">
           Registrarse
         </button>
-      </form>
+      </Form>
     </div>
   </section>
 </template>
 
 <script>
+import { Form, Field } from 'vee-validate';
+import * as Yup from 'yup';
+
+import { useRegisterStore } from '@/stores/register.store.js';
+
 export default {
+  components: {
+    Form,
+    Field
+  },
   data() {
     return {
-      showPassword01: false,
-      showPassword02: false,
-      valuePassword01: '',
-      valuePassword02: ''
+      newUser: {
+        name: '',
+        lastname: '',
+        email: '',
+        password: ''
+      },
+
+      typePassword01: 'password',
+      typePassword02: 'password',
+
+      registerStore: useRegisterStore(),
+
+      schema: Yup.object().shape({
+        name: Yup.string()
+          .required('El nombre es requerido.'),
+        lastname: Yup.string()
+          .required('El apellido es requerido.'),
+        email: Yup.string()
+          .required('El correo es requerido.')
+          .email('Ingrese un correo electrónico válido.'),
+        password01: Yup.string()
+          .required('La contraseña es requerida.')
+          .min(6, 'La contraseña de ser de al menos 6 caracteres'),
+        password02: Yup.string()
+          .required('Por favor, confirme la contraseña.')
+          .oneOf([Yup.ref('password01'), null], 'Las contraseñas deben coincidir'),
+        acceptTerms: Yup.string()
+          .oneOf(['off'], 'Debes aceptar los Términos y Condiciones'),
+      })
     };
-  }
+  },
 };
 </script>
 
 <style scoped>
-label:not(label[for="accept"]) {
+label:not(label[for="acceptTerms"]) {
   font-size: 0.75rem;
   color: #c4c4c4;
   transition: all 0.3s;
@@ -107,7 +168,7 @@ label:not(label[for="accept"]) {
   left: 15px;
 }
 
-input:not(:valid)+label:not(label[for="accept"]) {
+input:not(:valid)+label:not(label[for="acceptTerms"]) {
   font-size: 0.75rem;
   color: #c4c4c4;
   top: 20px;
@@ -115,13 +176,13 @@ input:not(:valid)+label:not(label[for="accept"]) {
   transition: all 0.3s;
 }
 
-input:focus+label:not(label[for="accept"]) {
+input:focus+label:not(label[for="acceptTerms"]) {
   top: 8px;
   font-size: 0.625rem;
   transition: all 0.3s;
 }
 
-input:valid+label:not(label[for="accept"]) {
+input:valid+label:not(label[for="acceptTerms"]) {
   top: 8px;
   font-size: 0.625rem;
   transition: all 0.3s;
@@ -146,5 +207,4 @@ input:valid+label:not(label[for="accept"]) {
   border: 3px solid #ffffff;
   box-shadow: 0 0 0 1px #222222;
   border-radius: 2px;
-}
-</style>
+}</style>
